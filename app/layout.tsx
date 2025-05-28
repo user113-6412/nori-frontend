df42ea3ace5ed11f675aaef42f7e021f4e542d3d
Victor Jarm <viktorasjarm92@gmail.com>
2025-05-28 11:07:17 +0700

    google tag fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,25 +70,28 @@ export default function RootLayout({
         {/* Google tag (gtag.js) */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17124083870" />
         
-        <Script id="google-ads" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17124083870');
+            gtag('config', 'AW-17124083870', {
+              'send_page_view': true,
+              'conversion_linker': true
+            });
           `}
         </Script>
 
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-17124083870');
-          `}
-        </Script>
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');`
+          }}
+        />
 
       </head>
       <body className={`${quicksand.className} antialiased`}>
@@ -96,7 +99,7 @@ export default function RootLayout({
           {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-17124083870"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
